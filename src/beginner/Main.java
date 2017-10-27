b093cd9ef66144113175b748908961ee3f528101
sanguk lee <pos236@gmail.com>
2017-10-27 12:36:43 +0900

    ws1 initial format

--- src/beginner/Main.java
+++ src/beginner/Main.java
@@ -3,7 +3,39 @@ package beginner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
+		// 1st exercise
+		// print hello world
 
+		
+		// 2nd exercise
+		// typing practice - chat bot
+		
+		
+		// Shortcuts
+		// alt, shift, ctrl movement
+		// suggestion, import, auto completion
+		// auto formatting, delete a line
+		
+		
+		// 3rd exercise
+		// chat bot: input
+		
+		
+		// 4th exercise
+		// interactive chat bot
+		// repeat input
+		// input: name, age
+		
+		
+		// loop for diamond
+		
+		
+		// summary
+		// command line
+		// eclipse and basic manual
+		// output, input
+		// String assignment, call
+		
+		// option. while loop
+	}
 }
