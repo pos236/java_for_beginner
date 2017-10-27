1987912521e6b91107bc7176fec8f22bf3f3a67c
sanguk lee <pos236@gmail.com>
2017-10-27 13:20:16 +0900

    ws2

--- src/beginner/Main.java
+++ src/beginner/Main.java
@@ -1,9 +1,44 @@
 package beginner;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+
+		// 1st exercise
+		// simple addition calculator
+		// if we haven't cover loop, do loop first
+		
+		
+		// 2nd exercise
+		//
+		// use += operator
+		
+		
+		// 3rd exercise
+		// subtraction, multiplication
+		
+		
+		// 4th exercise
+		// use double for division
+
+		
+		// 5th exercise
+		// use exit code to reach turn off code
+		// first condition usage
+
+		System.out.println("Turn on calculator");
+
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		System.out.println("current sum = " + sum);
+		while (true) {
+			sum = sum + sc.nextInt();
+			System.out.println("current sum = " + sum);
+		}
+
+		// System.out.println("Turn on calculator");
 	}
 
 }
