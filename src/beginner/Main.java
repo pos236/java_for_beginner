58a8e25ede639f5e00b5a70bc7b4207a4a188637
sanguk lee <pos236@gmail.com>
2017-10-29 10:15:28 +0900

    ws7

--- src/beginner/Main.java
+++ src/beginner/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
+	public static int DEFAULT_VALUE = 0;
+	
 	public static void main(String[] args) {
 		// grid length
 		int gridLength = 12;
@@ -13,7 +15,7 @@ public class Main {
 		// actually, it's not required since integer will be set 0 by default.
 		for(int inx = 0; inx<gridLength; inx++) {
 			for(int jnx =0; jnx < gridLength; jnx++) {
-				grid[inx][jnx] = 0;
+				grid[inx][jnx] = DEFAULT_VALUE;
 			}
 		}
 		
@@ -21,18 +23,12 @@ public class Main {
 		int[] pxy = new int[3];
 		while(true)	{
 			// print array
-			for(int inx = 0; inx<gridLength; inx++) {
-				for(int jnx =0; jnx < gridLength; jnx++) {
-					if(grid[inx][jnx] == 1)
-						System.out.print("o ");
-					else if(grid[inx][jnx] == 2)
-						System.out.print("x ");
-					else
-						System.out.print(". ");
-				}
-				System.out.print("\r");
+			printArray(gridLength, grid);
+			
+			if(isGameFinished(gridLength, grid)) {
+				System.out.println("Congrat!!");
+				break;
 			}
-			System.out.print("\r");
 			
 			// use array or three variables
 			pxy[0] = sc.nextInt();
@@ -45,6 +41,105 @@ public class Main {
 			else
 				grid[pxy[1]][pxy[2]] = 2;
 		}
+	}
+
+	private static boolean isGameFinished(int gridLength, int[][] grid) {
+		for(int inx = 0; inx < gridLength; inx++) {
+			for(int jnx = 0; jnx < gridLength; jnx++) {
+				int row = inx;
+				int column = jnx;
+				
+				if(
+					grid[row][column] != DEFAULT_VALUE
+					&&
+					( fiveToRight(gridLength, grid, row, column)
+					|| fiveToDown(gridLength, grid, row, column)
+					|| fiveToDiagonal(gridLength, grid, row, column)
+					|| fiveToSlash(gridLength, grid, row, column)
+					)	)
+					return true;
+			}
+		}
+		
+		return false;
+	}
+
+	private static boolean fiveToDiagonal(int gridLength, int[][] grid, int row, int column) {
+		int value = grid[row][column];
+		int count = 0;
+		while(count<5) {
+			if( row >= gridLength || column >= gridLength || grid[row][column] != value )
+				break;
+			else {
+				row++;
+				column++;
+				count++;
+			}	
+		}
+		
+		return count == 5 ? true: false;
+	}
+	
+	private static boolean fiveToSlash(int gridLength, int[][] grid, int row, int column) {
+		int value = grid[row][column];
+		int count = 0;
+		while(count<5) {
+			if( row >= gridLength || column <0 || grid[row][column] != value )
+				break;
+			else {
+				row++;
+				column--;
+				count++;
+			}	
+		}
+		
+		return count == 5 ? true: false;
+	}
+
+	private static boolean fiveToDown(int gridLength, int[][] grid, int row, int column) {
+		int value = grid[row][column];
+		int count = 0;
+		while(count<5) {
+			if( row >= gridLength || grid[row][column] != value )
+				break;
+			else {
+				row++;
+				count++;
+			}	
+		}
+		
+		return count == 5 ? true: false;
+	}
+
+	private static boolean fiveToRight(int gridLength, int[][] grid, int row, int column) {
+		int value = grid[row][column];
+		int count = 0;
+		while(count<5) {
+			if( column >= gridLength || grid[row][column] != value )
+				break;
+			else {
+				column++;
+				count++;
+			}	
+		}
+		
+		return count == 5 ? true: false;
+	}
+
+	private static void printArray(int gridLength, int[][] grid) {
+		for(int inx = 0; inx<gridLength; inx++) {
+			for(int jnx =0; jnx < gridLength; jnx++) {
+				if(grid[inx][jnx] == 1)
+					System.out.print("o ");
+				else if(grid[inx][jnx] == 2)
+					System.out.print("x ");
+				else
+					System.out.print(". ");
+			}
+			System.out.print("\r");
+		}
+		System.out.print("\r");
+		
 	}
 
 }
