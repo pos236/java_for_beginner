438a1a7c933a0ad37dc9b4471513cf059f6ec3aa
sanguk lee <pos236@gmail.com>
2017-10-31 08:28:25 +0900

    fibonacci

--- src/beginner/Main.java
+++ src/beginner/Main.java
@@ -3,7 +3,37 @@ package beginner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		int fibonacci = 9;
+		// 1 1 2 3 5 8 13 21 34
+		System.out.println(fibonacci(fibonacci));
 	}
 
+//	private static int fibonacci(int n) {
+//		if( n == 0)
+//			return 0;
+//		
+//		if( n == 1)
+//			return 1;
+//		
+//		return fibonacci(n-1) + fibonacci(n-2);
+//	}
+	
+	private static int fibonacci(int n) {
+		if( n == 0)
+			return 0;
+		
+		if( n == 1 || n == 2 )
+			return 1;
+		
+		int[] fibonacci = new int[n];
+		fibonacci[0] = 1;
+		fibonacci[1] = 1;
+		int index = 2;
+		while(index < n) {
+			fibonacci[index] = fibonacci[index-1] + fibonacci[index-2];
+			index++;
+		}
+		
+		return fibonacci[n-1];
+	}
 }
